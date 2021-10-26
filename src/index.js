70f93c64b768ea53abb77424cbec1f44687c6a54
yanchen01 <ychen01@bu.edu>
2021-10-26 12:14:12 -0400

    init express server

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const weatherRouter = require('./routes/weather');
+
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/weather', weatherRouter);
+
+app.get('*', (req, res) => {
+	res.status(403).send({
+		message: 'Invalid Access'
+	});
+});
